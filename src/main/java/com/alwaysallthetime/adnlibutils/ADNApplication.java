875b7f98917a4fe31dfb4fb9b06fd03b95260462
Rob Brambley <rrbrambley@gmail.com>
2013-10-20 14:48:51 -0700

    add base application class with convenience method for getting a context.

---
+++ src/main/java/com/alwaysallthetime/adnlibutils/ADNApplication.java
@@ -0,0 +1,18 @@
+package com.alwaysallthetime.adnlibutils;
+
+import android.app.Application;
+import android.content.Context;
+
+public class ADNApplication extends Application {
+    private static Context sContext;
+
+    public static Context getContext() {
+        return sContext;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sContext = getApplicationContext();
+    }
+}
