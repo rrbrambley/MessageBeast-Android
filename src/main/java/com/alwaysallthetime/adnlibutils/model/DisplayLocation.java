76590c3d14fce7c3d82fa199459e0174551709e5
Rob Brambley <rrbrambley@gmail.com>
2013-10-27 12:54:22 -0700

    create DisplayLocation from Geolocation object

--- src/main/java/com/alwaysallthetime/adnlibutils/model/DisplayLocation.java
+++ src/main/java/com/alwaysallthetime/adnlibutils/model/DisplayLocation.java
@@ -6,13 +6,16 @@ import java.util.HashMap;
 
 public class DisplayLocation {
     private String mName;
+    private String mFactualId;
     private double mLatitude;
     private double mLongitude;
 
     public static DisplayLocation fromCheckinAnnotation(Annotation checkin) {
         HashMap<String, Object> value = checkin.getValue();
         String locationName = (String) value.get("name");
-        return new DisplayLocation(locationName, (Double)value.get("latitude"), (Double)value.get("longitude"));
+        String factualId = (String) value.get("factual_id");
+        DisplayLocation loc = new DisplayLocation(locationName, factualId, (Double)value.get("latitude"), (Double)value.get("longitude"));
+        return loc;
     }
 
     public static DisplayLocation fromOhaiLocation(Annotation ohaiLocation) {
@@ -22,10 +25,14 @@ public class DisplayLocation {
         Double longitude = (Double) value.get("longitude");
         if(name == null) {
             if(latitude != null && longitude != null) {
-                name = getLatLongString(latitude.toString(), longitude.toString());
+                name = getLatLongString(latitude, longitude);
             }
         }
         return new DisplayLocation(name, (Double)value.get("latitude"), (Double)value.get("longitude"));
+    }
+
+    public static DisplayLocation fromGeolocation(Geolocation geolocation) {
+        return new DisplayLocation(geolocation.getName(), geolocation.getLatitude(), geolocation.getLongitude());
     }
 
     public DisplayLocation(double latitude, double longitude) {
@@ -36,6 +43,12 @@ public class DisplayLocation {
     public DisplayLocation(String name, double latitude, double longitude) {
         this(latitude, longitude);
         mName = name;
+    }
+
+    public DisplayLocation(String name, String factualId, double latitude, double longitude) {
+        this(latitude, longitude);
+        mName = name;
+        mFactualId = factualId;
     }
 
     public String getName() {
@@ -50,7 +63,11 @@ public class DisplayLocation {
         return mLongitude;
     }
 
-    private static String getLatLongString(String latitude, String longitude) {
-        return String.format("%s, %s", latitude, longitude);
+    public String getFactualId() {
+        return mFactualId;
+    }
+
+    private static String getLatLongString(double latitude, double longitude) {
+        return String.format("%s, %s", String.valueOf(latitude), String.valueOf(longitude));
     }
 }
