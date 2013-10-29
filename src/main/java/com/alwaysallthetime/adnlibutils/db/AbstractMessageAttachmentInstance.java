74b43764536946dbaab58d58a3d5efcaa4b100d4
Rob Brambley <rrbrambley@gmail.com>
2013-10-29 15:58:53 -0700

    Not sure if this helps anything, but keep message ids in order they were added

--- src/main/java/com/alwaysallthetime/adnlibutils/db/AbstractMessageAttachmentInstance.java
+++ src/main/java/com/alwaysallthetime/adnlibutils/db/AbstractMessageAttachmentInstance.java
@@ -1,13 +1,13 @@
 package com.alwaysallthetime.adnlibutils.db;
 
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public abstract class AbstractMessageAttachmentInstance {
-    protected HashSet<String> mMessageIds;
+    protected LinkedHashSet<String> mMessageIds;
 
     public AbstractMessageAttachmentInstance() {
-        mMessageIds = new HashSet<String>();
+        mMessageIds = new LinkedHashSet<String>();
     }
 
     public void addInstance(String messageId) {
@@ -22,7 +22,7 @@ public abstract class AbstractMessageAttachmentInstance {
         return mMessageIds.size();
     }
 
-    public Set<String> getMessageIds() {
+    public LinkedHashSet<String> getMessageIds() {
         return mMessageIds;
     }
 }
