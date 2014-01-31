996b893909e6c6275496261cb8c7e6df3068a6df
Rob Brambley <rrbrambley@gmail.com>
2014-01-30 18:09:43 -0800

    add methods for individual entity types, javadocs

--- src/main/java/com/alwaysallthetime/messagebeast/EntityStyler.java
+++ src/main/java/com/alwaysallthetime/messagebeast/EntityStyler.java
@@ -8,8 +8,71 @@ import com.alwaysallthetime.adnlib.data.Entities;
 
 import java.util.List;
 
+/**
+ * A utility for applying Character styles to entities.
+ */
 public class EntityStyler {
 
+    /**
+     * Get styled hashtags. If none exist, then the returned CharSequence has no styled spans.
+     *
+     * @param post the post or Message whose hashtags should be styled
+     * @param hashtagStyles the CharacterStyles to apply to the hashtags
+     * @return A CharSequence with styled spans
+     */
+    public static CharSequence getStyledHashtags(AbstractPost post, List<CharacterStyle> hashtagStyles) {
+        SpannableStringBuilder string = new SpannableStringBuilder(post.getText());
+
+        Entities entities = post.getEntities();
+        if(entities != null) {
+            applyStylesToEntities(string, entities.getHashtags(), hashtagStyles);
+        }
+        return string;
+    }
+
+    /**
+     * Get styled links. If none exist, then the returned CharSequence has no styled spans.
+     *
+     * @param post the post or Message whose links should be styled
+     * @param linkStyles the CharacterStyles to apply to the links
+     * @return A CharSequence with styled spans
+     */
+    public static CharSequence getStyledLinks(AbstractPost post, List<CharacterStyle> linkStyles) {
+        SpannableStringBuilder string = new SpannableStringBuilder(post.getText());
+
+        Entities entities = post.getEntities();
+        if(entities != null) {
+            applyStylesToEntities(string, entities.getLinks(), linkStyles);
+        }
+        return string;
+    }
+
+    /**
+     * Get styled mentions. If none exist, then the returned CharSequence has no styled spans.
+     *
+     * @param post the post or Message whose mentions should be styled
+     * @param mentionStyles the CharacterStyles to apply to the mentions
+     * @return A CharSequence with styled spans
+     */
+    public static CharSequence getStyledMentions(AbstractPost post, List<CharacterStyle> mentionStyles) {
+        SpannableStringBuilder string = new SpannableStringBuilder(post.getText());
+
+        Entities entities = post.getEntities();
+        if(entities != null) {
+            applyStylesToEntities(string, entities.getMentions(), mentionStyles);
+        }
+        return string;
+    }
+
+    /**
+     * Get styled entities. If none exist, then the returned CharSequence has no styled spans.
+     *
+     * @param post the post or Message whose entities should be styled
+     * @param mentionStyles the CharacterStyles to apply to the mentions. may be null.
+     * @param hashtagStyles the CharacterStyles to apply to the hashtags. may be null.
+     * @param linkStyles the CharacterStyles to apply to the links. may be null.
+     * @return A CharSequence with styled spans
+     */
     public static CharSequence getStyledText(AbstractPost post, List<CharacterStyle> mentionStyles, List<CharacterStyle> hashtagStyles, List<CharacterStyle> linkStyles) {
         SpannableStringBuilder string = new SpannableStringBuilder(post.getText());
 
